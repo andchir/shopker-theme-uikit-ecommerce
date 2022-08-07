95f17cccd9b4cba5e05d6df7bcb39b4875998cef
andrew <andchir@gmail.com>
2022-08-07 20:22:53 +0300

    Checkout page update

--- assets/uikit-ecommerce/scripts/checkout-page.js
+++ assets/uikit-ecommerce/scripts/checkout-page.js
@@ -1,4 +1,5 @@
 
+// Shopping cart edit
 if (document.getElementById('shoppingCartContainer')) {
     var shoppingCartEdit = new ShoppingCart({
         baseUrl: '/',
@@ -9,3 +10,23 @@ if (document.getElementById('shoppingCartContainer')) {
     });
 }
 
+// Checkout form
+if (document.getElementById('form-checkout-contacts')) {
+    const shippingSelect = document.getElementById('order_deliveryName');
+    function onCheckoutFormUpdate() {
+        const paymentSelect = document.getElementById('order_paymentName');
+        const sectionShipping = document.getElementById('form-checkout-shipping');
+        const checkoutFormDataPayment = document.getElementById('checkoutFormDataPayment');
+        const checkoutFormDataShipping = document.getElementById('checkoutFormDataShipping');
+        checkoutFormDataShipping.innerText = Array.from(shippingSelect.querySelectorAll('option'))[parseInt(shippingSelect.value)].innerText;
+        checkoutFormDataPayment.innerText = Array.from(paymentSelect.querySelectorAll('option'))[parseInt(paymentSelect.value)].innerText;
+        if (parseInt(shippingSelect.value) === 0) {
+            sectionShipping.style.display = 'none';
+        } else {
+            sectionShipping.style.display = 'block';
+            // TODO: update shopping cart
+        }
+    }
+    shippingSelect.addEventListener('change', onCheckoutFormUpdate);
+    onCheckoutFormUpdate();
+}
